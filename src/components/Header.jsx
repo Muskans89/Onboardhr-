362b79cd6baacc1a65ed441a387ba371770f87dc
Muskans89 <109502928+Muskans89@users.noreply.github.com>
2024-08-16 12:04:01 +0530

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom'; // Import NavLink from react-router-dom
-import logo from '../assets/logo.png'; // Correct import path for the logo
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,80 +14,63 @@ const Header = () => {
       <div className="container mx-auto px-8 py-4 flex justify-between items-center">
         {/* Logo */}
         <div className="flex items-center space-x-4">
-          <NavLink to="/"> {/* Use NavLink to route to home */}
-            <img src={logo} alt="logo" className="h-16 w-28 -ml-10" /> {/* Adjusted logo size and margin */}
+          <NavLink to="/">
+            <img src={logo} alt="logo" className="h-18 w-28 -ml-10" />
           </NavLink>
         </div>
 
         {/* Desktop Navigation */}
-        <nav className="hidden md:flex space-x-12 items-center mr-4"> {/* Adjusted right margin */}
+        <nav className="hidden md:flex space-x-12 items-center mr-4">
           <NavLink
             to="/services"
             className={({ isActive }) =>
               isActive
-                ? "text-gray-900 font-medium text-2xl px-4 py-2 rounded-md"
-                : "text-gray-600 hover:text-gray-900 font-medium text-2xl px-4 py-2 rounded-md"
+                ? "text-gray-900 font-medium text-xl px-4 py-2 rounded-md"
+                : "text-gray-600 hover:text-gray-900 font-medium text-xl px-4 py-2 rounded-md"
             }
           >
             Services
           </NavLink>
 
-          <div className="relative group">
-            <NavLink
-              to="/expertise"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-gray-900 font-medium text-2xl px-4 py-2 rounded-md flex items-center"
-                  : "text-gray-600 hover:text-gray-900 font-medium text-2xl px-4 py-2 rounded-md flex items-center"
-              }
-            >
-              Expertise <svg className="ml-1 w-5 h-5 fill-current" viewBox="0 0 20 20"><path d="M7 7l3 3 3-3H7z" /></svg>
-            </NavLink>
-            {/* Dropdown Menu */}
-            <div className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
-              <NavLink to="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Startups & Scale Ups</NavLink>
-              <NavLink to="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Enterprise</NavLink>
-              <NavLink to="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Tech</NavLink>
-              <NavLink to="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Sales & Marketing</NavLink>
-              <NavLink to="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Finance, HR & Ops</NavLink>
-            </div>
-          </div>
-
-          <div className="relative group">
-            <NavLink
-              to="/about"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-gray-900 font-medium text-2xl px-4 py-2 rounded-md flex items-center"
-                  : "text-gray-600 hover:text-gray-900 font-medium text-2xl px-4 py-2 rounded-md flex items-center"
-              }
-            >
-              About Us <svg className="ml-1 w-5 h-5 fill-current" viewBox="0 0 20 20"><path d="M7 7l3 3 3-3H7z" /></svg>
-            </NavLink>
-            {/* Dropdown Menu */}
-            <div className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
-              <NavLink to="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Locations</NavLink>
-              <NavLink to="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Who We Are</NavLink>
-            </div>
-          </div>
-
           <NavLink
-            to="/blog"
+            to="/expertise"
             className={({ isActive }) =>
               isActive
-                ? "text-gray-900 font-medium text-2xl px-4 py-2 rounded-md"
-                : "text-gray-600 hover:text-gray-900 font-medium text-2xl px-4 py-2 rounded-md"
+                ? "text-gray-900 font-medium text-xl px-4 py-2 rounded-md"
+                : "text-gray-600 hover:text-gray-900 font-medium text-xl px-4 py-2 rounded-md"
             }
           >
-            Blog
+            Industry Practices
+          </NavLink>
+
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive
+                ? "text-gray-900 font-medium text-xl px-4 py-2 rounded-md"
+                : "text-gray-600 hover:text-gray-900 font-medium text-xl px-4 py-2 rounded-md"
+            }
+          >
+            About Us
+          </NavLink>
+
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              isActive
+                ? "text-gray-900 font-medium text-xl px-4 py-2 rounded-md"
+                : "text-gray-600 hover:text-gray-900 font-medium text-xl px-4 py-2 rounded-md"
+            }
+          >
+            Contact Us
           </NavLink>
 
           {/* Show "Contact Us" button only on desktop */}
           <NavLink
             to="/contact"
-            className="bg-customBlue text-white px-5 py-2 rounded-md hover:bg-blue-600 font-medium text-lg hidden md:inline-block mr-0"
+            className="bg-customBlue  hover:bg-customYellow text-white px-5 py-2 rounded-md  font-medium text-lg hidden md:inline-block mr-0"
           >
-            Contact Us
+           Book a Call
           </NavLink>
         </nav>
 
@@ -95,7 +78,7 @@ const Header = () => {
         <div className="md:hidden">
           <button onClick={toggleMenu} className="text-gray-700 focus:outline-none">
             <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> {/* Hamburger icon */}
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
           </button>
         </div>
@@ -106,15 +89,15 @@ const Header = () => {
             <div className="w-2/3 h-full bg-white shadow-lg p-8">
               <button onClick={toggleMenu} className="text-gray-700 focus:outline-none mb-8">
                 <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path> {/* Close icon */}
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
                 </svg>
               </button>
               <nav className="flex flex-col space-y-4">
                 <NavLink to="/" className="text-gray-700 hover:text-gray-900 font-medium text-lg">Home</NavLink>
                 <NavLink to="/services" className="text-gray-700 hover:text-gray-900 font-medium text-lg">Services</NavLink>
-                <NavLink to="#" className="text-gray-700 hover:text-gray-900 font-medium text-lg">Expertise</NavLink>
-                <NavLink to="#" className="text-gray-700 hover:text-gray-900 font-medium text-lg">About Us</NavLink>
-                <NavLink to="#" className="text-gray-700 hover:text-gray-900 font-medium text-lg">Blog</NavLink>
+                <NavLink to="/expertise" className="text-gray-700 hover:text-gray-900 font-medium text-lg">Industry Practices</NavLink>
+                <NavLink to="/about" className="text-gray-700 hover:text-gray-900 font-medium text-lg">About Us</NavLink>
+                <NavLink to="/contact" className="text-gray-700 hover:text-gray-900 font-medium text-lg">Contact Us</NavLink>
               </nav>
             </div>
           </div>
